f1af84e7269e76fdb4318b2a4cdcc8180ba3a6e7
Volker Kempert <volker.kempert@almedso.de>
2020-02-22 08:59:11 +0100

    Add qemu machine named quirin and run script

---
+++ scripts/run-quirin-linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+qemu-system-x86_64 \
+    -device virtio-scsi-pci,id=scsi \
+    -drive file=${1:-"ams-image-gui-dev"}-quirin.wic,id=root-img,if=none,format=raw,cache=none \
+    -device scsi-hd,drive=root-img \
+    -no-reboot \
+    -serial mon:stdio --enable-kvm -device virtio-vga,xres=480,yres=800 \
+    -vga none -cpu host -m 4096 \
+    -usb -device usb-kbd -usb -device usb-tablet
